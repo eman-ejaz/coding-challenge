f023e57831060a047e17b14db6d225da9f925364
Muhammad Eman Ejaz <mejaz@siammakro.co.th>
2024-12-08 22:58:52 +1100

    chore: add constants and update types for account categories and value types

---
+++ src/constants.ts
@@ -0,0 +1,24 @@
+export const ACCOUNT_CATEGORY_REVENUE = "revenue";
+export const ACCOUNT_CATEGORY_EXPENSE = "expense";
+
+export const ACCOUNT_VALUE_TYPE_DEBIT = "debit";
+export const ACCOUNT_VALUE_TYPE_CREDIT = "credit";
+
+export enum ACCOUNT_CATEGORY {
+  REVENUE = "revenue",
+  EXPENSE = "expense",
+  ASSETS = "assets",
+  LIABILITY = "liability",
+}
+
+export enum ACCOUNT_VALUE_TYPE {
+  DEBIT = "debit",
+  CREDIT = "credit",
+}
+
+export enum ACCOUNT_TYPE {
+  SALES = "sales",
+  CURRENT = "current",
+  BANK = "bank",
+  CURRENT_ACCOUNTS_RECEIVABLE = "current_accounts_receivable",
+}
--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,29 @@
+import {
+  ACCOUNT_CATEGORY,
+  ACCOUNT_TYPE,
+  ACCOUNT_VALUE_TYPE,
+} from "./constants";
+
 /**
  * Represents an account item with details about its category, type, value type, and total value.
  */
 export interface AccountItem {
-  account_category: string;
-  account_type: string;
-  value_type: string;
+  account_category: ACCOUNT_CATEGORY;
+  account_type: ACCOUNT_TYPE;
+  value_type: ACCOUNT_VALUE_TYPE;
   total_value: number;
 }
 
 /**
  * Represents the structure of the data object.
- * 
+ *
  * @interface Data
  * @property {AccountItem[]} data - An array of AccountItem objects.
  */
 export interface Data {
   data: AccountItem[];
+  object_category: string;
+  connection_id: string;
+  user: string;
+  object_creation_date: string;
 }
